ec864c2ece36c44a12a880d96620442b94ba03b7
moryw <matt@skitskat.xyz>
2018-07-03 12:05:15 +0700

    Add comment to missionControl and add 2 new files.

--- 1-missionControl.js
+++ 1-missionControl.js
@@ -1,4 +1,4 @@
-const Airplane = require('./1-airplane.js');
+const Airplane = require('./1-airplane.js'); //module import property name doesnt have to match the exported var name
 
 function displayAirplane() {
   console.log(Airplane.myAirplane);
