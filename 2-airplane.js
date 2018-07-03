86dcc5ce3672cc395b6d4ec4318b1aef7e1c5aa6
moryw <matt@skitskat.xyz>
2018-07-03 12:14:01 +0700

    Add empty object and make mod.exp an object.

--- 2-airplane.js
+++ 2-airplane.js
@@ -1,1 +1,13 @@
 const Airplane = {};
+
+module.exports = {
+  myAirplane: "CloudJet",
+  //this is the way the course wants it
+  displayAirplane: function() {
+    return this.myAirplane;
+  }
+  //this is how i think it should be
+  // displayAirplane() {
+  //   return this.myAirplane;
+  // }
+};
